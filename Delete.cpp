f9f9a71268c0f4c4714423e950c0ab9475368999
FREE! <ydexir@gmail.com>
2025-10-03 08:24:43 +0800

    Delete.cpp

--- Delete.cpp
+++ Delete.cpp
@@ -1,6 +1,38 @@
 #include <stdio.h>
 #define x 10
 
+int getSize();
+void input(int a[x], int NoOfItems);
+int Delete(int a[x], int num, int NoOfItems, int found);
+
+int main(){
+	int a[x];
+	int num;
+	int NoOfItems;
+	int found = 0;
+	
+	
+	NoOfItems = getSize();
+	input(a, NoOfItems);
+	
+	printf("What number you want to delete? ");
+	scanf("%d", &num);
+	
+	found = Delete(a, num, NoOfItems, found);
+	
+	if (found){
+		--NoOfItems;
+		printf("This is the new list after delete:\n");
+		for (int i = 0; i < NoOfItems; i++){
+			printf("%d\n", a[i]);
+		}
+	} else{
+		printf("Number %d not found in the list.\n", num);
+	}
+
+	return 0;
+}
+
 int getSize(){
 	int NoOfItems;
 	
@@ -32,34 +64,6 @@ int Delete(int a[x], int num, int NoOfItems, int found){
 		i++;
 	}while(!found && i <NoOfItems);
 	return found;
-}
-
-int main(){
-	int a[x];
-	int num;
-	int NoOfItems;
-	int found = 0;
-	
-	
-	NoOfItems = getSize();
-	input(a, NoOfItems);
-	
-	printf("What number you want to delete? ");
-	scanf("%d", &num);
-	
-	found = Delete(a, num, NoOfItems, found);
-	
-	if (found){
-		--NoOfItems;
-		printf("This is the new list after delete:\n");
-		for (int i = 0; i < NoOfItems; i++){
-			printf("%d\n", a[i]);
-		}
-	} else{
-		printf("Number %d not found in the list.\n", num);
-	}
-
-	return 0;
 }
 
 
