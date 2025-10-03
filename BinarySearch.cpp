5f82bf0992ee3cb738fa3a0f3bcde046f93ae326
FREE! <ydexir@gmail.com>
2025-10-03 08:50:53 +0800

    BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,53 +1,60 @@
 #include <stdio.h>
-#define MaxIndex 10
 
-void input(int a[MaxIndex]);
-int binarySearch(int x, int a[MaxIndex]);
+int inputMax();
+void inputArray(int a[], int MaxIndex);
+int binarySearch(int x, int a[], int MaxIndex);
 
-int main(){
-	int a[MaxIndex];
-	int x;
-	
-	input(a);
-	
-	printf("Enter number you want to search: ");
-	scanf("%d", &x);
-	
-	int result = binarySearch(x, a);
-	
-	if (result != -1){
-		printf("Number found at Index %d ", result);
-	}else{
-		printf("Number is not found");
-	}
-	
-	return 0;
+int main() {
+    int MaxIndex = inputMax();
+    int a[MaxIndex];
+    int x;
+
+    inputArray(a, MaxIndex);
+
+    printf("Enter number you want to search: ");
+    scanf("%d", &x);
+
+    int result = binarySearch(x, a, MaxIndex);
+
+    if (result != -1) {
+        printf("Number found at index %d\n", result);
+    } else {
+        printf("Number not found.\n");
+    }
+
+    return 0;
 }
 
-void input(int a[MaxIndex]){
-	for (int i = 0; i < MaxIndex; i++){
-		printf("Input num: ");
-		scanf("%d", &a[i]);
-	}
+int inputMax() {
+    int MaxIndex;
+    printf("How many numbers do you want to input? ");
+    scanf("%d", &MaxIndex);
+    return MaxIndex;
 }
 
-int binarySearch(int x, int a[MaxIndex]){
-	int k = 0;
-	int m = MaxIndex -1;
-	int Midpoint;
-	
-	while (k <= m){
-		Midpoint = (k + m) / 2;
-		
-		if (x == a[Midpoint]){
-			return Midpoint;
-		}
-		else if (x > a[Midpoint]) {
-			k = Midpoint + 1;
-		}
-		else {
-			m = Midpoint -1;
-		}
-	}
-	return -1;
+void inputArray(int a[], int MaxIndex) {
+    printf("Enter the numbers in sorted order (for binary search to work):\n");
+    for (int i = 0; i < MaxIndex; i++) {
+        printf("Input number %d: ", i + 1);
+        scanf("%d", &a[i]);
+    }
+}
+
+int binarySearch(int x, int a[], int MaxIndex) {
+    int k = 0;
+    int m = MaxIndex - 1;
+    int Midpoint;
+
+    while (k <= m) {
+        Midpoint = (k + m) / 2;
+
+        if (x == a[Midpoint]) {
+            return Midpoint;
+        } else if (x > a[Midpoint]) {
+            k = Midpoint + 1;
+        } else {
+            m = Midpoint - 1;
+        }
+    }
+    return -1;
 }
