bd4e5a830903f595007a202cab22ac14151771d8
FREE! <ydexir@gmail.com>
2025-10-03 08:26:09 +0800

    Insert.cpp

--- Insert.cpp
+++ Insert.cpp
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #define t 10
 
+int getSize();
+void input(int a[t], int NoOfItems);
+int insert(int a[t], int NoOfItems, int position, int numToInsert);
+
+int main() {
+    int a[t];
+    int position, numToInsert, NoOfItems;
+
+    NoOfItems = getSize();
+    input(a, NoOfItems);
+
+    printf("What number you want to Insert? ");
+    scanf("%d", &numToInsert);
+
+    printf("On what Position you want to input? ");
+    scanf("%d", &position);
+
+    NoOfItems = insert(a, NoOfItems, position, numToInsert);
+
+    printf("The new list:\n");
+    for (int i = 0; i < NoOfItems; i++) {
+        printf("%d\n", a[i]);
+    }
+
+    return 0;
+}
+
 int getSize() {
     int NoOfItems;
     printf("Enter numbers of item you want to input (max %d): ", t);
@@ -25,26 +52,4 @@ int insert(int a[t], int NoOfItems, int position, int numToInsert) {
     return NoOfItems;
 }
 
-int main() {
-    int a[t];
-    int position, numToInsert, NoOfItems;
-
-    NoOfItems = getSize();
-    input(a, NoOfItems);
-
-    printf("What number you want to Insert? ");
-    scanf("%d", &numToInsert);
-
-    printf("On what Position you want to input? ");
-    scanf("%d", &position);
-
-    NoOfItems = insert(a, NoOfItems, position, numToInsert);
-
-    printf("The new list:\n");
-    for (int i = 0; i < NoOfItems; i++) {
-        printf("%d\n", a[i]);
-    }
-
-    return 0;
-}
 
