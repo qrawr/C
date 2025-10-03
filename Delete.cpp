2087b8ac9636941e7e2d40886262299f351800f3
FREE! <ydexir@gmail.com>
2025-10-03 10:23:41 +0800

    Delete.cpp

--- Delete.cpp
+++ Delete.cpp
@@ -2,12 +2,12 @@
 
 int getSize();
 void inputArray(int a[], int NoOfItems);
-int Delete(int a[], int Delete, int NoOfItems, int found);
+int Delete(int a[], int deleteNum, int NoOfItems, int found);
 
 int main(){
 	int NoOfItems = getSize();
 	int a[NoOfItems];
-	int Delete;
+	int deleteNum;
 	int found = 0;
 	
 	inputArray(a, NoOfItems);
@@ -15,7 +15,7 @@ int main(){
 	printf("What number you want to delete? ");
 	scanf("%d", &Delete);
 	
-	found = Delete(a, Delete, NoOfItems, found);
+	found = Delete(a, deleteNum, NoOfItems, found);
 	
 	if (found){
 		--NoOfItems;
@@ -24,7 +24,7 @@ int main(){
 			printf("%d\n", a[i]);
 		}
 	} else{
-		printf("Number %d not found in the list.\n", num);
+		printf("Number %d not found in the list.\n", deleteNum);
 	}
 
 	return 0;
@@ -39,16 +39,16 @@ int getSize(){
 
 void inputArray(int a[], int NoOfItems){
 	for (int i = 0; i < NoOfItems; i++){
-		printf("Input number %d: ", i + 1");
+		printf("Input number %d: ", i + 1);
 		scanf("%d", &a[i]);
 	}
 }
 
-int Delete(int a[], int num, int NoOfItems, int found){
+int Delete(int a[], int deleteNum, int NoOfItems, int found){
 	int i = 0;
 	
 	do {
-		if (Delete == a[i]){
+		if (deleteNum == a[i]){
 			for (int n = i + 1; n < NoOfItems - 1; n++)
 			a[n-1] = a[n];
 			found = 1;
